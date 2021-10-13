05e7d4c5466e4bb3286b7c829bd02bed93026f80
Incahuasi123 <46612691+Incahuasi123@users.noreply.github.com>
2021-10-12 23:08:12 -0300

    Add files via upload

---
+++ Contador.ino
@@ -0,0 +1,142 @@
+int a=1,b=2,c=3,d=4,e=5,f=6,g=7;
+int Boton=8,Contador=0;
+
+void setup() {
+ pinMode (a,OUTPUT);
+ pinMode (b,OUTPUT);
+ pinMode (c,OUTPUT);
+ pinMode (d,OUTPUT);
+ pinMode (e,OUTPUT);
+ pinMode (f,OUTPUT);
+ pinMode (g,OUTPUT);
+ pinMode (Boton,INPUT);
+
+  
+
+
+}
+
+void loop() {
+
+  if(digitalRead(Boton)){
+    Contador++;
+  }
+
+  switch (Contador){
+
+    case 0:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,LOW);
+
+    break;
+
+    case 1:
+    digitalWrite(a,LOW);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,LOW);
+    digitalWrite(e,LOW);
+    digitalWrite(f,LOW);
+    digitalWrite(g,LOW);
+
+    break;
+
+    case 2:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,LOW);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,LOW);
+    digitalWrite(g,HIGH);
+
+    break;
+
+    case 3:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,LOW);
+    digitalWrite(f,LOW);
+    digitalWrite(g,HIGH);
+
+    break;
+
+    case 4:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,LOW);
+    digitalWrite(e,LOW);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+
+    break;
+
+    case 5:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,LOW);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,LOW);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+
+    break;
+
+    case 6:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,LOW);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+
+    break;
+
+    case 7:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,LOW);
+    digitalWrite(e,LOW);
+    digitalWrite(f,LOW);
+    digitalWrite(g,LOW);
+
+    break;
+
+    case 8:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+
+    break;
+
+    case 9:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,LOW);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+
+    break;
+
+    
+  }
+
+}
+
+
